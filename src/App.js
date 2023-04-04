ac27010005393058ccffb27dbbae31b629285955
dmooredidit <dandre.moore2@gmail.com>
2023-04-04 12:10:33 -0700

    playerOne if and else statements done. Must check to see the clicks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import React, { useState } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { useState } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  
-  const [turn, setTurn] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
-  const [currentPlayer, setCurrentPlayer] = useState("x") 
+  const [turn, setTurn] = useState(true);
 
+  const [playerOne, setPlayerOne] = useState(true);
 
   const handleGamePlay = (clickedSquare) => {
-    let updateSquare = [...squares]
+    let updateSquare = [...squares];
 
-    updateSquare[clickedSquare]= currentPlayer
-    setSquares(updateSquare)
+    // updateSquare[clickedSquare]= currentPlayer
+    if (playerOne && clickedSquare === null) {
+      clickedSquare = "X";
+    } else {
+      !playerOne;
+    }
+    setSquares(updateSquare);
     // setTurn(false)
-    console.log(clickedSquare)
-  }
-  const switchPlayer = () => {
-    
-  }
-  
+    console.log(clickedSquare);
+  };
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className="board">
-      {
-        squares.map((value, index) => {
-          return <Square 
-            value={value}
-            index={index}
-            key={index}
-            handleGamePlay={handleGamePlay}
+        {squares.map((value, index) => {
+          return (
+            <Square
+              value={value}
+              index={index}
+              key={index}
+              handleGamePlay={handleGamePlay}
             />
-        }
-        )
-      }
+          );
+        })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
